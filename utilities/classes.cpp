d04eedc1ea13ab75ba9679908e31ad0e03fae611
Jiaqi <jiaqili@kth.se>
2018-04-12 00:05:03 +0200

    functions added

--- utilities/classes.cpp
+++ utilities/classes.cpp
@@ -1,157 +1,147 @@
-#include <vector>
-#include <set>
-#include <map>
-#include <string>
-#include <iostream>
-#include <string>
-#include <regex>
-#include <fstream>
 #include <dirent.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <set>
+#include <string>
+#include <vector>
 using namespace std;
 
 /**
- * 
- * the class of a function. it contains its name and the functions it uses in its definition
- * 
+ *
+ * the class of a function. it contains its name and the functions it uses in
+ * its definition
+ *
  */
-class AFunc
-{
-    private:
-    string myName;
-    vector<string> usedFunc;
+class AFunc {
+ private:
+  string myName;
+  vector<string> usedFunc;
 
-    public:
-    /**
-     * constructor, giving a name of a function instance
- */
-    AFunc(string str)
-    {
-        myName = str;
-    }
-    /**
-     * returns the name of the function
- */
-    string getMyName()
-    {
-        return myName;
-    }
-    /**
-     * add functions used by this function
- */
-    void addUsedFunc(string str)
-    {
-        usedFunc.push_back(str);
-    }
-    /**
-     * show used functions on terminal
- */
-    void showUsedFunc()
-    {
-        for(vector<string>::iterator v = usedFunc.begin(); v!=usedFunc.end();v++)
-        cout << *v << endl;
-    }
+ public:
+  /**
+   * constructor, giving a name of a function instance
+   */
+  AFunc(string str) { myName = str; }
+  /**
+   * returns the name of the function
+   */
+  string getMyName() { return myName; }
+  /**
+   * add functions used by this function
+   */
+  void addUsedFunc(string str) { usedFunc.push_back(str); }
+  /**
+   * show used functions on terminal
+   */
+  void showUsedFunc() {
+    for (vector<string>::iterator v = usedFunc.begin(); v != usedFunc.end();
+         v++)
+      cout << *v << endl;
+  }
 };
 
 /**
- * 
- * the class of a file, it contains the functions it defines and the files it relies on
- * 
+ *
+ * the class of a file, it contains the functions it defines and the files it
+ * relies on
+ *
  */
 
-class AFile
-{
-  private:
-    string myName;
-    vector<AFile> reliedFiles;
-    // set<string> myFunctions;
-    map<int,string> myFunctions;
-    vector<AFunc> myFunc;
+class AFile {
+ private:
+  string myName;
+  vector<AFile> reliedFiles;
+  // set<string> myFunctions;
+  map<int, string> myFunctions;
+  vector<AFunc> myFunc;
 
-  public:
+ public:
   /**
    * friend function, allowing it to have access to private variables
- */
-    friend void findReliances(string filePath);
-    /**
-     * constructor
- */
-    AFile(string name)
-    {
-        myName = name;
+   */
+  friend void findReliances(string filePath);
+  /**
+   * constructor
+   */
+  AFile(string name) { myName = name; }
+  /**
+   * add relied files
+   */
+  void addReliedFiles(string name) {
+    AFile relied(name);
+    reliedFiles.push_back(relied);
+  }
+  /**
+   * whether the relied file name has been added before
+   */
+  bool isAdded(string str) {
+    for (vector<AFile>::iterator v = reliedFiles.begin();
+         v != reliedFiles.end(); v++) {
+      if (v->myName == str) return true;
     }
-    /**
-     * add relied files
- */
-    void addReliedFiles(string name)
-    {
-        AFile relied(name);
-        reliedFiles.push_back(relied);
+    return false;
+  }
+  /**
+   * add functions it defines
+   */
+  void addMyFunc(string funcName, int count) {
+    myFunctions.insert(pair<int, string>(count, funcName));
+  }
+  /**
+   * whether the definition of a function has been added
+   */
+  bool funcIsAdded(int count, string str) {
+    map<int, string>::iterator v = myFunctions.find(count);
+    if (v != myFunctions.end()) {
+      if (myFunctions[count] == str) return true;
     }
-    /**
-     * whether the relied file name has been added before
- */
-    bool isAdded(string str)
-    {
-        for(vector<AFile>::iterator v = reliedFiles.begin();v!=reliedFiles.end();v++)
-        {
-            if(v->myName==str)
-            return true;
-        }
-        return false;
+    return false;
+  }
+  /**
+   * display its relied files
+   */
+  void displayReliedFiles() {
+    cout << "Relied Files:" << endl;
+    for (vector<AFile>::iterator v = reliedFiles.begin();
+         v != reliedFiles.end(); v++) {
+      cout << v->myName << endl;
     }
-    /**
-     * add functions it defines
- */
-    void addMyFunc(string funcName, int count)
-    {
-        myFunctions.insert(pair<int,string>(count,funcName));
+    cout << endl;
+  }
+  /**
+   * display functions it defines
+   */
+  void displayMyFunctions() {
+    cout << "Contained Functions:" << endl;
+    for (map<int, string>::iterator v = myFunctions.begin();
+         v != myFunctions.end(); v++) {
+      cout << (*v).second << " in line " << (*v).first << endl;
+      cout << "Used Functions: " << endl
+           << "例子1 defined in file 1" << endl
+           << "例子2 defined in file 2" << endl
+           << "例子3 defined in file 3" << endl;
+      displayUsedFuncs((*v).second);
     }
-    /**
-     * display its relied files
- */
-    void displayReliedFiles()
-    {
-        cout << "Relied Files:" << endl;
-        for (vector<AFile>::iterator v = reliedFiles.begin(); v != reliedFiles.end();v++)
-        {
-            cout << v->myName <<endl;
-        }
-        cout << endl ;
+    cout << endl;
+  }
+  /**
+   * displays functions the defined functions uses
+   */
+  void displayUsedFuncs(string str) {
+    for (vector<AFunc>::iterator v = myFunc.begin(); v != myFunc.end(); v++) {
+      if (v->getMyName() == str) v->showUsedFunc();
     }
-    /**
-     * display functions it defines
- */
-    void displayMyFunctions()
-    {
-        cout << "Contained Functions:" << endl;
-        for (map<int,string>::iterator v = myFunctions.begin(); v != myFunctions.end(); v++)
-        {
-            cout << (*v).second << " in line " << (*v).first << endl;
-            cout << "Used Functions: " << endl << "例子1" << endl << "例子2" << endl << "例子3" << endl;
-            displayUsedFuncs((*v).second);
-        }
-        cout << endl;
+  }
+  /**
+   * create function instances
+   */
+  void copyTomyFunc() {
+    for (map<int, string>::iterator v = myFunctions.begin();
+         v != myFunctions.end(); v++) {
+      AFunc aFunc((*v).second);
+      myFunc.push_back(aFunc);
     }
-    /**
-     * displays functions the defined functions uses
- */
-    void displayUsedFuncs(string str)
-    {
-        for (vector<AFunc>::iterator v = myFunc.begin(); v != myFunc.end(); v++)
-        {
-            if(v->getMyName()==str)
-            v->showUsedFunc();
-        }
-    }
-    /**
-     * create function instances
- */
-    void copyTomyFunc()
-    {
-        for (map<int,string>::iterator v = myFunctions.begin(); v != myFunctions.end(); v++)
-        {
-            AFunc aFunc((*v).second);
-            myFunc.push_back(aFunc);
-        }
-    }
+  }
 };
--- utilities/findFunctions.cpp
+++ utilities/findFunctions.cpp
@@ -1,221 +1,231 @@
 #include "classes.cpp"
 /**
- * 
+ *
  * splitPath to get directory name and file name
- * 
+ *
  */
 
-void splitPath(const string &str, string &dir, string &ownname)
-{
-    size_t found;
-    // cout << "Splitting: " << str << endl;
-    found = str.find_last_of("/\\");
-    dir = str.substr(0, found);
-    ownname = str.substr(found + 1);
-    // cout << " folder: " << str.substr(0, found) << endl;
-    // cout << " file: " << str.substr(found + 1) << endl;
+void splitPath(const string &str, string &dir, string &ownname) {
+  size_t found;
+  // cout << "Splitting: " << str << endl;
+  found = str.find_last_of("/\\");
+  dir = str.substr(0, found);
+  ownname = str.substr(found + 1);
+  // cout << " folder: " << str.substr(0, found) << endl;
+  // cout << " file: " << str.substr(found + 1) << endl;
 }
 /**
- * 
- * 
+ *
+ *
  * read all files in a directory
- * 
+ *
  */
-void read_directory(const string &name, vector<string> &v)
-{
-    DIR* dirp = opendir(name.c_str());
-    struct dirent * dp;
-    while ((dp = readdir(dirp)) != NULL) {
-        v.push_back(dp->d_name);
-    }
-    closedir(dirp);
+void read_directory(const string &name, vector<string> &v) {
+  DIR *dirp = opendir(name.c_str());
+  struct dirent *dp;
+  while ((dp = readdir(dirp)) != NULL) {
+    v.push_back(dp->d_name);
+  }
+  closedir(dirp);
 }
 /**
- * 
+ *
  * find relied files in a file.
- * 
+ *
  */
-void findReliedFiles(string str, AFile &afile)
-{
-    regex regNote("\\s*(\\/|\\*).*"), fileExp("#include\\s*(<|\"|')(.*)(>|\"|')\\s*");
-    smatch sm;
-    if (regex_search(str, sm, fileExp))
-        if (!regex_match(str, regNote))
-            if(!afile.isAdded(sm[2]))
-            {
-                afile.addReliedFiles(sm[2]);
-            }
+void findReliedFiles(string str, AFile &aFile) {
+  regex regNote("\\s*(\\/|\\*).*"),
+      fileExp("#include\\s*(<|\"|')(.*)(>|\"|')\\s*");
+  smatch sm;
+  if (regex_search(str, sm, fileExp))
+    if (!regex_match(str, regNote))
+      if (!aFile.isAdded(sm[2])) {
+        aFile.addReliedFiles(sm[2]);
+      }
 }
 /**
- * 
+ *
  * find functions defined in a files
- * 
+ *
  */
 
-bool findFuncs(string str, AFile &afile, int count)
-{
-    regex regNote("\\s*(\\/|\\*).*"), regFunc("(\\s+|\\.)([a-z]+\\w*(?=\\s*\\(.*\\)\\s*\\{))");
-    set<string> notIncluded {"if","for","while"};
-    smatch sm;
-    if (regex_search(str, sm, regFunc))
-        if (!regex_match(str, regNote))
-            if (!notIncluded.count(sm[2]))
-            {
-                // cout << str << endl;
-                // cout << sm[2] << endl;
-                afile.addMyFunc(sm[2],count);
-                return true;
-            }
-    return false;
+bool findFuncs(string str, AFile &aFile, int count) {
+  regex regNote("\\s*(\\/|\\*).*"),
+      regFunc("(\\s+|\\.)([a-z]+\\w*(?=\\s*\\(.*\\)\\s*\\{))");
+  set<string> notIncluded{"if", "for", "while"};
+  smatch sm;
+  if (regex_search(str, sm, regFunc))
+    if (!regex_match(str, regNote))
+      if (!notIncluded.count(sm[2])) {
+        // cout << str << endl;
+        // cout << sm[2] << endl;
+        aFile.addMyFunc(sm[2], count);
+        return true;
+      }
+  return false;
 }
 /**
- * 
+ *
  * find functions used by a function
- * 
+ * if this line has a function in aFile adding it to aFunc
  */
-bool findUsedFuncs(string str, AFunc &aFunc)
-{
-    regex regNote("\\s*(\\/|\\*).*"), regFunc("(\\s+|\\.)([a-z]+\\w*(?=\\s*\\(.*\\)))");
-    set<string> notIncluded {"if","for","while"};
-    smatch sm;
-    if (regex_search(str, sm, regFunc))
-        if (!regex_match(str, regNote))
-            if (!notIncluded.count(sm[2]))
-            {
-                // cout << str << endl;
-                // cout << sm[2] << endl;
-                aFunc.addUsedFunc(sm[2]);
-                return true;
-            }
-    return false;
+bool findUsedFuncs(string str, AFunc &aFunc) {
+  regex regNote("\\s*(\\/|\\*).*"),
+      regFunc("(\\s+|\\.)([a-z]+\\w*(?=\\s*\\(.*\\)))");
+  set<string> notIncluded{"if", "for", "while"};
+  smatch sm;
+  if (regex_search(str, sm, regFunc))
+    if (!regex_match(str, regNote))
+      if (!notIncluded.count(sm[2])) {
+        // cout << str << endl;
+        // cout << sm[2] << endl;
+        aFunc.addUsedFunc(sm[2]);
+        return true;
+      }
+  return false;
 }
-/**
- * 
- * find the code lump in the definition of a function
- * 
- */
-// void findFuncDef(string str)
-// {
-//      while (getline(infile, str))
-//         {
-//             ++count;
-//             string twoLines;
-//             twoLines = str;
-//             findReliedFiles(str, afile);
-//             if(getline(infile, str))
-//             {
-//                 ++count;
-//                 findReliedFiles(str, afile);
-//                 twoLines = twoLines + "\n" + str;
-//             }
-//             else break;
-//             findFuncs(twoLines, afile);
-//         }
-//         infile.close();
-// }
 
 /**
- * 
+ * find the beginning of a code definition
+ */
+bool findFuncDefBegin(int count, string str, AFile &aFile) {
+  regex regNote("\\s*(\\/|\\*).*"),
+      regFunc("(\\s+|\\.)([a-z]+\\w*(?=\\s*\\(.*\\)))");
+  set<string> notIncluded{"if", "for", "while"};
+  smatch sm;
+  if (regex_search(str, sm, regFunc))
+    if (!regex_match(str, regNote))
+      if (!notIncluded.count(sm[2]))
+        if (aFile.funcIsAdded(count, sm[2])) return true;
+  return false;
+}
+
+/**
+ *
+ * find the codes in the definition of a function
+ * between {}
+ */
+void findFuncDefStr(string path, AFile aFile, string &code) {
+  int count = 0;
+  string str;
+  ifstream infile;
+  regex e("\\{");
+  smatch sm;
+  infile.open(path);
+  while (getline(infile, str)) {
+    count++;
+    if (findFuncDefBegin(count, str, aFile)) {
+      int symCount;
+      char c;
+      if (regex_search(str, sm, e))
+        symCount++;
+      else {
+        while (symCount == 0) {
+          infile.get(c);
+          if (c == '\n') count++;
+          if (c == '{') symCount++;
+        }
+      }
+      while (symCount != 0) {
+        infile.get(c);
+        if (c == '{') symCount++;
+        if (c == '}') symCount--;
+        if (c == '\n') count++;
+        code = code + c;
+      }
+      cout << code << endl;
+    }
+  }
+  infile.close();
+}
+/**
+ *
  * find all reliances of a file
- * 
+ *
  */
 void findReliances(string filePath) {
-    
-    ifstream infile;
-    infile.open(filePath);
-    if(infile)
-    {
-    cout << "Analysing: " << filePath << endl << endl;    
+  ifstream infile;
+  infile.open(filePath);
+  if (infile) {
+    cout << "Analysing: " << filePath << endl << endl;
     string str;
-    AFile afile(filePath);
-    string dir,ownname;
+    AFile aFile(filePath);
+    string dir, ownname;
     int count = 0;
-    
-        while (getline(infile, str))
-        {
-            ++count;
-            string twoLines;
-            twoLines = str;
-            findReliedFiles(str, afile);
-            if(!findFuncs(twoLines, afile, count))
-            {
-            if(getline(infile, str))
-            {
-                ++count;
-                findReliedFiles(str, afile);
-                if(!findFuncs(str, afile, count))
-                {
-                    twoLines = twoLines + "\n" + str;
-                    findFuncs(twoLines, afile, count-1);
-                }
-            }
-            else break;
-            }
-        }
-        infile.close();
-        count = 0;
-        infile.open(filePath);
-        getline(infile, str);
-        ++count;
-        while (getline(infile, str))
-        {
-            ++count;
-            string twoLines;
-            twoLines = str;
-            findReliedFiles(str, afile);
-            if(!findFuncs(twoLines, afile, count))
-            {
-            if(getline(infile, str))
-            {
-                ++count;
-                findReliedFiles(str, afile);
-                if(!findFuncs(str, afile, count))
-                {
-                    twoLines = twoLines + "\n" + str;
-                    findFuncs(twoLines, afile, count-1);
-                }
-            }
-            else break;
-            
-            }
-        }
+
+    while (getline(infile, str)) {
+      ++count;
+      string twoLines;
+      twoLines = str;
+      findReliedFiles(str, aFile);
+      if (!findFuncs(twoLines, aFile, count)) {
+        if (getline(infile, str)) {
+          ++count;
+          findReliedFiles(str, aFile);
+          if (!findFuncs(str, aFile, count)) {
+            twoLines = twoLines + "\n" + str;
+            findFuncs(twoLines, aFile, count - 1);
+          }
+        } else
+          break;
+      }
+    }
     infile.close();
 
+    count = 0;
+    infile.open(filePath);
+    getline(infile, str);
+    ++count;
+    while (getline(infile, str)) {
+      ++count;
+      string twoLines;
+      twoLines = str;
+      findReliedFiles(str, aFile);
+      if (!findFuncs(twoLines, aFile, count)) {
+        if (getline(infile, str)) {
+          ++count;
+          findReliedFiles(str, aFile);
+          if (!findFuncs(str, aFile, count)) {
+            twoLines = twoLines + "\n" + str;
+            findFuncs(twoLines, aFile, count - 1);
+          }
+        } else
+          break;
+      }
+    }
+    infile.close();
 
+    aFile.displayReliedFiles();
+    aFile.displayMyFunctions();
 
-    afile.displayReliedFiles();
-    afile.displayMyFunctions();
+    // string code;
+    // findFuncDefStr(filePath, aFile, code);
+
     splitPath(filePath, dir, ownname);
     cout << "There are " << count << " lines in " << filePath << endl << endl;
-    
-    for(vector<AFile>::iterator it = afile.reliedFiles.begin(); it!= afile.reliedFiles.end();it++)
-    {
-        string path;
-        path = dir + "/" + it->myName;
-        // cout << "Next path: " << path << endl;
-        findReliances(path);
+
+    for (vector<AFile>::iterator it = aFile.reliedFiles.begin();
+         it != aFile.reliedFiles.end(); it++) {
+      string path;
+      path = dir + "/" + it->myName;
+      // cout << "Next path: " << path << endl;
+      findReliances(path);
     }
-    }
+  }
 }
 
-
-int main(int argc, char *argv[]) 
-{
-    if(argc>=2)
-    {
-        // vector<string> files;
-        string dir;
-        findReliances(argv[1]);
-        // read_directory(dir,files);
-        // for(int i=0;i<files.size();i++)
-        // {
-        //     cout  << " Path: " << files[i] <<endl;
-        // }
-    }
-    // cout << "Argument number: " << argc << endl;
-    // for(int i=0;i<argc;i++)
-    // cout <<  "Arguments: " << argv[i] << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc >= 2) {
+    // vector<string> files;
+    string dir;
+    findReliances(argv[1]);
+    // read_directory(dir,files);
+    // for(int i=0;i<files.size();i++)
+    // {
+    //     cout  << " Path: " << files[i] <<endl;
+    // }
+  }
+  // cout << "Argument number: " << argc << endl;
+  // for (int i = 0; i < argc; i++) cout << "Arguments: " << argv[i] << endl;
+  return 0;
 }
-
-
